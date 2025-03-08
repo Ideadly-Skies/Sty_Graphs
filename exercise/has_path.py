f165101ee166e324f187db1c3001d0756b2c5123
Ideadly-Skies <Obie.kal22@gmail.com>
2025-03-08 18:35:53 +0700

    feat: added implementation for has_path exercise

---
+++ exercise/has_path.py
@@ -0,0 +1,34 @@
+# use recursive depth-first traversal
+def has_path(graph, src, dst):
+    stack = [ src ]
+
+    while stack:
+        # set last element as node
+        node = stack[-1]
+        
+        # check if dst exists
+        if node == dst:
+            return True
+
+        # pop from the end of stack 
+        stack.pop()
+
+        # append neighbor to stack
+        for neighbor in graph[node]:
+            stack.append(neighbor)
+    
+    # the stack does not contain the dst element
+    return False
+
+
+if __name__ == "__main__":
+    graph = {
+        'f': ['g', 'i'],
+        'g': ['h'],
+        'h': [],
+        'i': ['g', 'k'],
+        'j': ['i'],
+        'k': []
+    }
+
+    print(has_path(graph, 'i', 'h')) # True
